d9b584d5d1b1723408eb221fe8b8dd9af0ecb8b6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-05 14:02:23 +0600

    feat: add layout components and pages for navigation, including About, Blogs, Contact, Login, Register, and Not Found

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Blogs</h1>
+            <p>This is the blogs page.</p>
+        </div>
+    );
+};
+
+export default BlogPage;
