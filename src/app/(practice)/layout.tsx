ba4f1d3c71840597c4f55aa7ff6ef9368898223c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-06 21:03:35 +0600

    feat: add marketing and sales components with layout integration

---
+++ src/app/(practice)/layout.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+
+const PracticeLayout = ({
+    children,
+    marketingSlot,
+    salesSlot
+}: {
+    children: React.ReactNode,
+    marketingSlot: React.ReactNode,
+    salesSlot: React.ReactNode
+}) => {
+    return (
+        <div>
+            <nav className='flex gap-10 m-8'>
+                <Link className='hover:underline' href="/development">Development</Link>
+                <Link className='hover:underline' href="/testing">Testing</Link>
+                <Link className='hover:underline' href="/marketing">Marketing</Link>
+                <Link className='hover:underline' href="/marketing/settings">Settings</Link>
+                <Link className='hover:underline' href="/sales">Sales</Link>
+            </nav>
+
+            <div className='flex gap-10 m-8'>
+                {marketingSlot}
+                {salesSlot}
+            </div>
+
+            {children}
+        </div>
+    );
+};
+
+export default PracticeLayout;
