68645c41da97429c7b39c2491ac856341f37daf6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-08 03:20:16 +0600

    feat: enhance proxy authentication logic for role-based access control

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,6 +5,7 @@ import { Roles } from "./constants/role";
 export async function proxy(request: NextRequest) {
     let isAuthenticated = false;
     let isAdmin = false;
+    const pathName = request.nextUrl.pathname;
     const { data } = await userService.getSession();
 
     if (data) {
@@ -12,13 +13,26 @@ export async function proxy(request: NextRequest) {
         isAdmin = data.user.role === Roles.admin;
     }
 
+    // User is not authenticated and trying to access protected routes
     if (!isAuthenticated) {
         return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // User is authenticated but role = ADMIN
+    // trying to access user routes without user role
+    if (isAdmin && pathName.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/admin-dashboard', request.url));
+    }
+
+    // User is authenticated but role = USER
+    // trying to access ADMIN routes without ADMIN role
+    if (!isAdmin && pathName.startsWith('/admin-dashboard')) {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/dashboard'],
+    matcher: ['/dashboard', '/admin-dashboard'],
 }
