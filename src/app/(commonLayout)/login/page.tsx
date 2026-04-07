4b9d4aaa31b4b18d05ce300047e9041c19025ef5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-07 15:40:09 +0600

    feat: implement login and registration forms with responsive design

--- src/app/(commonLayout)/login/page.tsx
+++ src/app/(commonLayout)/login/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { LoginForm } from "@/components/login-form"
 
-const Login = () => {
-    return (
-        <div>
-            <h1>Login page</h1>
-        </div>
-    );
-};
-
-export default Login;
+export default function Login() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
